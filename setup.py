00c8290b781790d5302eb01ba474a1d71cc02883
cosmonaut <guavajellyfish@yahoo.com>
2011-03-25 21:55:53 -0600

    Added README and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-coaddx1d',
+      version='0.1.0',
+      description='python-coaddx1d - generalized coaddition for COS x1d files',
+      author='Nico Nell',
+      author_email='nicholas.nell@colorado.edu',
+      url='http://casa.colorado.edu/~nell/site/software/python-coaddx1d',
+      packages=['coaddx1d'],
+     )
